03b168c852253ebd9f8318fd49152774c3234f38
Doruk Cem <doruk.cem@gmail.com>
2023-12-22 11:59:39 +0300

    HTTP Exceptions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # uvicorn main:app --reload
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -21,14 +21,14 @@ def get_todos():
 @app.post("/add-todo")
 def add_todo(todo : Item):
    if todo.name in todos:
-      return {f"Error: {todo} already in todos list"}
+      raise HTTPException( status_code= status.HTTP_400_BAD_REQUEST, detail=f"Error: Todo '{todo.name}' already exists" )
    todos[todo.name] = todo
    return {"message" : f"Added todo: {todo.name}"}
 
 @app.post("/toggle-todo/{todo}")
 def toggle_todo(todo_name : str):
    if todo_name not in todos: 
-      return {f"Error: {todo_name} not in todos list"}
+      raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="Item not found")
    todos[todo_name].is_done = not todos[todo_name].is_done
    return {f"{todo_name}" : f"{todos[todo_name].is_done}"}
 
@@ -37,4 +37,4 @@ def remove_todo(todo_name : str):
    if todo_name in todos:
       del todos[todo_name]
       return {"message" : f"Removed {todo_name}"}
-   return {"message" : f"{todo_name} was not in todos"}
+   raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="Item not found")
